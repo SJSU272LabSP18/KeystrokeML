5697e2b89e529725df711c352138f622f11440ce
Huy Huynh <huyhuynh1228@gmail.com>
2018-05-03 15:49:42 -0700

    Capture time stamp keyup keydown

---
+++ CS272_project/src/keyInput.java
@@ -0,0 +1,26 @@
+import java.lang.*;
+public class keyInput {
+
+    int keyUpTime;
+    public int getKeyUp() {
+        return keyUpTime;
+    }
+
+    public void setKeyUpTime(int keyUpTime) {
+        this.keyUpTime = keyUpTime;
+    }
+
+    int keyDownTime;
+
+    public int getKeyDownTime() {
+        return keyDownTime;
+    }
+
+    public void setKeyDownTime(int keyDownTime) {
+        this.keyDownTime = keyDownTime;
+    }
+    public void saveToCSV(String CSVFilePath)
+    {
+
+    }
+}
